8a6a581b0a48f1ab4b50b73b2a12a9b9538ed5f6
Theonaw <theonawro@gmail.com>
2024-11-12 19:54:35 +0100

    Commit d'ajout du sexeIMG

--- IMG  (indice de masse grasse)/img.js
+++ IMG  (indice de masse grasse)/img.js
@@ -8,79 +8,81 @@ function img() {
             let tab = [0, 10.83];
             var res = (1.20 * poids / (taille * taille)) + (0.23 * age) - (tab[i] * 1) - 5.4;
             document.getElementById('final').value = res.toFixed(2);
-            if (sexe[0].checked = true){
+            if (sexe[i].value == 'femme') {
+                sexeIMG = 1;
+            } else {
                 sexeIMG = 0;
-            }
+            };
             if (age >= 65) {
-                if(res.toFixed(2) < 25){
+                if (res.toFixed(2) < 25 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 25 && res.toFixed(2) < 30){
+                } else if (res.toFixed(2) >= 25 - sexeIMG && res.toFixed(2) < 30 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 30 && res.toFixed(2) < 35){
+                } else if (res.toFixed(2) >= 30 - sexeIMG && res.toFixed(2) < 35 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 35 && res.toFixed(2) < 45){
+                } else if (res.toFixed(2) >= 35 - sexeIMG && res.toFixed(2) < 45 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 45){
+                } else if (res.toFixed(2) > 45 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
             } else if (age > 18 && age < 24) {
-                if(res.toFixed(2) < 20){
+                if (res.toFixed(2) < 20 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 20 && res.toFixed(2) < 25){
+                } else if (res.toFixed(2) >= 20 - sexeIMG && res.toFixed(2) < 25 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 25 && res.toFixed(2) < 30){
+                } else if (res.toFixed(2) >= 25 - sexeIMG && res.toFixed(2) < 30 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 30 && res.toFixed(2) < 40){
+                } else if (res.toFixed(2) >= 30 - sexeIMG && res.toFixed(2) < 40 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 40){
+                } else if (res.toFixed(2) > 40 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
-            }else if (age > 25 && age < 34) {
-                if(res.toFixed(2) < 21){
+            } else if (age > 25 && age < 34) {
+                if (res.toFixed(2) < 21 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 21 && res.toFixed(2) < 26){
+                } else if (res.toFixed(2) >= 21 - sexeIMG && res.toFixed(2) < 26 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 26 && res.toFixed(2) < 31){
+                } else if (res.toFixed(2) >= 26 - sexeIMG && res.toFixed(2) < 31 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 31 && res.toFixed(2) < 41){
+                } else if (res.toFixed(2) >= 31 - sexeIMG && res.toFixed(2) < 41 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 41){
+                } else if (res.toFixed(2) > 41 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
-            }else if (age > 35 && age < 44) {
-                if(res.toFixed(2) < 22){
+            } else if (age > 35 && age < 44) {
+                if (res.toFixed(2) < 22 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 22 && res.toFixed(2) < 27){
+                } else if (res.toFixed(2) >= 22 - sexeIMG && res.toFixed(2) < 27 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 27 && res.toFixed(2) < 32){
+                } else if (res.toFixed(2) >= 27 - sexeIMG && res.toFixed(2) < 32 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 32 && res.toFixed(2) < 42){
+                } else if (res.toFixed(2) >= 32 - sexeIMG && res.toFixed(2) < 42 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 42){
+                } else if (res.toFixed(2) > 42 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
-            }else if (age > 45 && age < 54) {
-                if(res.toFixed(2) < 23){
+            } else if (age > 45 && age < 54) {
+                if (res.toFixed(2) < 23 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 23 && res.toFixed(2) < 28){
+                } else if (res.toFixed(2) >= 23 - sexeIMG && res.toFixed(2) < 28 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 28 && res.toFixed(2) < 33){
+                } else if (res.toFixed(2) >= 28 - sexeIMG && res.toFixed(2) < 33 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 33 && res.toFixed(2) < 43){
+                } else if (res.toFixed(2) >= 33 - sexeIMG && res.toFixed(2) < 43 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 43){
+                } else if (res.toFixed(2) > 43 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
-            }else if (age > 55 && age < 64) {
-                if(res.toFixed(2) < 24){
+            } else if (age > 55 && age < 64) {
+                if (res.toFixed(2) < 24 - sexeIMG) {
                     document.getElementById('final2').value = "Insuffisance pondérale"
-                }else if(res.toFixed(2) >= 24 && res.toFixed(2) < 29){
+                } else if (res.toFixed(2) >= 24 - sexeIMG && res.toFixed(2) < 29 - sexeIMG) {
                     document.getElementById('final2').value = "Poids normal"
-                }else if(res.toFixed(2) >= 29 && res.toFixed(2) < 34){
+                } else if (res.toFixed(2) >= 29 - sexeIMG && res.toFixed(2) < 34 - sexeIMG) {
                     document.getElementById('final2').value = "Surpoids"
-                }else if(res.toFixed(2) >= 34 && res.toFixed(2) < 44){
+                } else if (res.toFixed(2) >= 34 - sexeIMG && res.toFixed(2) < 44 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité"
-                }else if(res.toFixed(2) > 44){
+                } else if (res.toFixed(2) > 44 - sexeIMG) {
                     document.getElementById('final2').value = "Obésité importante"
                 };
             };
